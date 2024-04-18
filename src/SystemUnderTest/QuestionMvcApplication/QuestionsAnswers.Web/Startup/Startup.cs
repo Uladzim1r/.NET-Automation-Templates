4b901bc279f6308e310d3a48a37d5fa5585d9e0e
Uladzim1r <lameikauladzimir@outlook.com>
2024-04-18 11:31:00 +0200

    Add test project

---
+++ src/SystemUnderTest/QuestionMvcApplication/QuestionsAnswers.Web/Startup/Startup.cs
@@ -0,0 +1,107 @@
+﻿using Abp.AspNetCore;
+using Abp.Castle.Logging.Log4Net;
+using Abp.EntityFrameworkCore;
+using Castle.Facilities.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using QuestionsAnswers.EntityFrameworkCore;
+using QuestionsAnswers.Identity;
+using QuestionsAnswers.Web.Authentication.JwtBearer;
+using QuestionsAnswers.Web.Configuration;
+using System;
+using Microsoft.OpenApi.Models;
+
+namespace QuestionsAnswers.Web.Startup
+{
+    public class Startup
+    {
+    
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        private readonly IConfigurationRoot _appConfiguration;
+        public Startup(IWebHostEnvironment env)
+        {
+            _hostingEnvironment = env;
+            _appConfiguration = env.GetAppConfiguration();
+
+        }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            //Configure DbContext
+            services.AddAbpDbContext<QuestionsAnswersDbContext>(options =>
+            {
+                DbContextOptionsConfigurer.Configure(options.DbContextOptions, options.ConnectionString);
+            });
+
+            services.AddControllersWithViews(options =>
+            {
+                options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+            }).AddNewtonsoftJson();
+
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "AbpZeroTemplate API", Version = "v1" });
+                options.DocInclusionPredicate((docName, description) => true);
+            });
+
+
+            IdentityRegistrar.Register(services);
+            AuthConfigurer.Configure(services, _appConfiguration);
+
+
+            //Configure Abp and Dependency Injection
+            return services.AddAbp<QuestionsAnswersWebModule>(options =>
+            {
+                //Configure Log4Net logging
+                options.IocManager.IocContainer.AddFacility<LoggingFacility>(
+                    f => f.UseAbpLog4Net().WithConfig(
+                        _hostingEnvironment.IsDevelopment()
+                            ? "log4net.config"
+                            : "log4net.Production.config"
+                        )
+                );
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseAbp(); //Initializes ABP framework.
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseSwagger();
+            //Enable middleware to serve swagger - ui assets(HTML, JS, CSS etc.)
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "AbpZeroTemplate API V1");
+            }); //URL: /swagger 
+
+            app.UseJwtTokenMiddleware();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
